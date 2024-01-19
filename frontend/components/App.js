ffd8d272c2aa106fee2ffdefad3635c6263bf70b
Percouple <calebsevan@gmail.com>
2024-01-19 16:31:52 -0600

    finishing-sprint-challenge-without-clearing-all-tests

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Character from './Character'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
@@ -7,12 +7,59 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  let [people, setPeople] = useState([]);
+  let [planets, setPlanets] = useState([]);
+  
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    axios.get(urlPeople)
+      .then((res) => {
+        return res.data
+      })
+      .then((people) => {
+        setPeople(people)
+      })
+      .catch(() => {
+        console.log("Error: Couldn't find people array endpoint")
+      })
+  }, [])
+
+  useEffect(() => {
+    axios.get(urlPlanets)
+      .then((res) => {
+        return res.data
+      })
+      .then(planets => {
+        setPlanets(planets)
+      })
+      .catch(() =>
+        console.error("Error: Couldn't find Planet array endpoint")
+      )
+  }, []);
+
+  // Assign planet names over planet Id's
+  const homeworldMap = new Map(planets.map((planet) => [planet.id, planet.name]))
+  for (let i = 0; i < people.length; i++) {
+    const homeworldId = people[i].homeworld;
+
+    if (homeworldMap.has(homeworldId)) {
+      people[i].homeworld = homeworldMap.get(homeworldId);
+    }
+  }
+
+  console.log(people)
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+        people.map((person, key) => {
+          return <Character name={person.name} homeworld={person.homeworld} key={key}/>
+        })
+      }
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ name, homeworld }) { // ❗ Add the props
+
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  let [toggledOn, setToggledOn] = useState('false');
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggleP = () => setToggledOn(!toggledOn)
+
   return (
-    <div>
+    <div className={"character-card"} onClick={toggleP}>
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 className={"character-name"}>{name}</h3>
+      {toggledOn ? <p>Planet:
+        <span className={"character-planet"} > {homeworld}</span>
+      </p> : ''}
     </div>
   )
 }
